1e1906099c00c6486caa5e05f9aaa70d64f0c5ee
Abhishek Airan <cubicfammy@gmail.com>
2024-09-12 16:15:02 +0530

    commit 0.1
    Added:
    - /suggestion

---
+++ src/commands/communication/suggestion.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder , EmbedBuilder } = require("discord.js")
+const { Channels } = require("../../config.json")
+
+module.exports = {
+    data: new SlashCommandBuilder().setName("suggestion").setDescription("Suggest something to the server").addStringOption(option => 
+        option.setName('message')
+              .setDescription('The suggestion message')
+              .setRequired(true) // Make it required or false if not mandatory
+    ),
+    async execute(interaction){
+        const suggestionChannel = await interaction.guild.channels.fetch(Channels.suggestion)
+        const embed = new EmbedBuilder().setAuthor({name: interaction.user.displayName, iconURL: interaction.user.avatarURL()}).setDescription(interaction.options.getString('message'))
+        const msg = await suggestionChannel.send({embeds: [embed]});
+        await msg.react('👍')
+        await msg.react('👎')
+        await interaction.reply({content:"Your Suggestion has been submitted",ephemeral: true})
+    }
+}
