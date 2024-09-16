85ae8a5f7877909eb19c4b530803c074be24aa20
Abhishek Airan <cubicfammy@gmail.com>
2024-09-16 22:34:49 +0530

    commit 1.0.4.1
    Some minor changes

--- src/events/test.js
+++ src/events/test.js
@@ -4,12 +4,12 @@ module.exports = {
     name: Events.MessageCreate,
     once: false,
     async execute(e){
-        e.client.emit("guildMemberAdd",{
-            user: {
-              tag: 'NewPerson#1234',
-            },
-          })
-        // console.log(e)
+        // e.client.emit("guildMemberAdd",{
+        //     user: {
+        //       tag: 'NewPerson#1234',
+        //     },
+        //   })
+        // // console.log(e)
 
     }
 }
